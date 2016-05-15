1335592542c0f9374e4cf08a596d3566e4299d79
Martin Flamm <mail@martinflamm.de>
2016-05-15 21:52:31 +0200

    added boostrap, styled landing page

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// Custom Scripts
+var forms = require('./forms/tag');
+
 var app = express();
 
 // view engine setup
