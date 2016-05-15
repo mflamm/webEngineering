64ff164292ea26a6731afd52cc6583a1532c2aca
Martin Flamm <mail@martinflamm.de>
2016-05-15 17:10:52 +0200

    added new routes for decovery and tagging page, added new jade templates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var options = {
+    title: 'Geo Location App'
+  };
+  res.render('index', options);
+});
+
+/* GET tagging page. */
+router.get('/tagging', function (req, res, next) {
+  var options = {
+    title: 'Geo Location Tagging'
+  };
+  res.render('tagging', options);
+});
+
+/* GET discover page. */
+router.get('/discovery', function (req, res, next) {
+  var options = {
+    title: 'Geo Location Discovery',
+    locations: [{
+      lat : 49.013790,
+      long : 8.404435,
+      name : 'castle',
+      hash : '#sight'
+    },{
+      lat : 49.013790,
+      long : 8.390071,
+      name : 'iwi',
+      hash : '#edu'
+    }]
+  };
+  res.render('discovery', options);
 });
 
 module.exports = router;
